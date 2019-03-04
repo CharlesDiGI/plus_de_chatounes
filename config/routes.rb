f1f97b18fc116a6ea98c9619c3232e2f405882ae
quentinvdcdc <quentin.vandecandelaere@gmail.com>
2019-03-04 13:48:28 +0100

    creation devise and items

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+	root 'items#index'
+  devise_for :users
+  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
