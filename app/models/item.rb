20b4a384c0644613ccdb601ad232f699285984ec
quentinvdcdc <quentin.vandecandelaere@gmail.com>
2019-03-04 18:59:27 +0100

    suppress comments

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
 	# VALIDATES
-	# validates :title, presence: true
-	# validates :description, presence: true, length { in: 5..40 }
-	# validates :price, presence: true, numericality: { greater_than: 0 }
-	# validates :image_url, presence: true
+	validates :title, presence: true
+	validates :description, presence: true, length { in: 5..40 }
+	validates :price, presence: true, numericality: { greater_than: 0 }
+	validates :image_url, presence: true
 end
