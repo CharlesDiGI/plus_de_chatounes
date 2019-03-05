f7ffc8b7e67f1c7d06377dd679b845b42ac08e7b
CharlesDiGI <charles.digiampietro@gmail.com>
2019-03-05 16:15:59 +0100

    update routes to nest join table & item

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-	root 'items#index'
-  devise_for :users
-  resources :items
+  root 'items#index'
+  
+  devise_for :users do
+    resources :carts
+  end
+
+  resources :items do
+    resources :join_table_cart_items
+  end
+
   resources :good_orders
-  resources :carts
-  resources :join_table_cart_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
