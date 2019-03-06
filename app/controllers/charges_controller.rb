731294411dee166804b5e1a574708d38b05f59ef
bonna023 <jerbonnardel@gmail.com>
2019-03-06 13:12:02 +0100

    création d'un paiement ok

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,7 +4,7 @@ class ChargesController < ApplicationController
 
 	def create
 	  # Amount in cents
-	  @amount = 500
+	  @amount = params[:cart_amount]
 
 	  customer = Stripe::Customer.create({
 	    email: params[:stripeEmail],
