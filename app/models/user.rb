604956e9f221649cd8e393c358034e06dc22fe74
CharlesDiGI <charles.digiampietro@gmail.com>
2019-03-06 10:56:26 +0100

    env file with mailjet api keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  after_create :welcome_send
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,5 +7,27 @@ class User < ApplicationRecord
 
   has_one :cart
   has_many :orders
+
+
+  def welcome_send
+    # UserMailer.welcome_email(self).deliver_now
+variable = Mailjet::Send.create(messages: [{
+    'From'=> {
+        'Email'=> 'charles.digiampietro@gmail.com',
+        # 'Name'=> 'Mailjet Pilot'
+    },
+    'To'=> [
+        {
+            'Email'=> 'passenger1@mailjet.com',
+            'Name'=> 'passenger 1'
+        }
+    ],
+    'Subject'=> 'Your email flight plan!',
+    'TextPart'=> 'Dear passenger 1, welcome to Mailjet! May the delivery force be with you!',
+    'HTMLPart'=> '<h3>Dear passenger 1, welcome to Mailjet!</h3><br />May the delivery force be with you!'
+}]
+)
+p variable.attributes['Messages']
+  end
   
 end
