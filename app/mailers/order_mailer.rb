71b129f2a746a0aec65ac8aadfbdfb03f4bf7e3b
CharlesDiGI <charles.digiampietro@gmail.com>
2019-03-06 12:35:00 +0100

    creation of order mailer, views and test

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,29 @@
+class OrderMailer < ApplicationMailer
+	
+	def order_recap(order)
+		@order = order
+		@user = @order.user
+		@url = "https://les-4-chatons-fantastiques.herokuapp.com/sign_in"
+
+	mail(
+    	from: "charles.digiampietro@gmail.com",
+          subject: "Thanks for your order",
+      to: @user.email,
+      delivery_method_options: { version: 'v3.1' }
+    )
+
+	end
+
+	def inform_admin(order)
+		@order = order
+		@user = @order.user
+
+	mail(
+    	from: "charles.digiampietro@gmail.com",
+          subject: "New Order from #{@user.first_name} #{@user.last_name} ",
+      to: "admin_les_4_chatons@yopmail.com",
+      delivery_method_options: { version: 'v3.1' }
+    )
+	end
+
+end
