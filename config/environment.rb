f9791a3c3147ec8592c4cd883cf1c529df3b44bb
CharlesDiGI <charles.digiampietro@gmail.com>
2019-03-06 13:59:18 +0100

    changed to SendGrid

--- config/environment.rb
+++ config/environment.rb
@@ -14,3 +14,13 @@ Rails.application.initialize!
 #   :authentication => :plain,
 #   :enable_starttls_auto => true
 # }
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_LOGIN'],
+  :password => ENV['SENDGRID_PWD'],
+  :domain => 'monsite.fr',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
