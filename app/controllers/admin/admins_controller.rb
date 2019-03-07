3e21d0e0a0539bc86ea99e626d6cf6c24b8ec3fb
Andrea <granger.ndr@gmail.com>
2019-03-07 14:53:31 +0100

    interface admin im progress

---
+++ app/controllers/admin/admins_controller.rb
@@ -0,0 +1,4 @@
+class Admin::AdminsController < ApplicationController
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,11 @@ Rails.application.routes.draw do
   resources :carts
   resources :orders
   resources :charges
+
+  namespace :admin do
+    resources :items
+    resources :admins, only: [:index]
+    root 'admins#index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
