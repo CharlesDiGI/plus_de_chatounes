999b299e66732e4197805f946a1278ca83202f86
CharlesDiGI <charles.digiampietro@gmail.com>
2019-03-07 19:56:45 +0100

    fixed bug on routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,6 @@ Rails.application.routes.draw do
     root 'admins#index'
   end
 
-  get '*path' => redirect('/')
+  # get '*path' => redirect('/')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
